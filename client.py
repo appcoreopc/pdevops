49200bd427df6f8227049b45bb81d708da66324b
jeremy woo <kepungmoin@hotmail.com>
2019-10-03 19:10:01 +1300

    added client and server

---
+++ client.py
@@ -0,0 +1,11 @@
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://localhost:9001"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("Hello world!")
+        await websocket.recv()
+
+asyncio.get_event_loop().run_until_complete(hello())
+

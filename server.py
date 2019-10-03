9acad33fdd2a9c8d7a5133bad2d95209e59f06d8
jeremy woo <kepungmoin@hotmail.com>
2019-10-03 20:49:19 +1300

    added stuff

--- server.py
+++ server.py
@@ -1,11 +1,29 @@
 import asyncio
+import datetime
+import random
 import websockets
+import http
 
-async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+# async def health_check(path, request_headers):
+ 
+#     if path == "/health/":
+#         print(type(path))
+#         print(type(request_headers))
+#         while True:
+#             now = datetime.datetime.utcnow().isoformat() + "Z"
+#             #await websocket.send(now)
+#             await asyncio.sleep(random.random() * 3)
 
-start_server = websockets.serve(echo, "localhost", 9001)
+
+async def time(websocket, path):
+    print("currentpath")
+    print(path)    
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + "Z"
+        await websocket.send(now)
+        await asyncio.sleep(random.random() * 3)
+
+start_server = websockets.serve(time, "127.0.0.1", 9001)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()

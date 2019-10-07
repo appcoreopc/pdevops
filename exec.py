ab317ecd26868242f616a45cc8fb2b1009e9fa11
jeremy woo <kepungmoin@hotmail.com>
2019-10-07 20:53:35 +1300

    status update

--- exec.py
+++ exec.py
@@ -1,8 +1,15 @@
 from tasks import add
 
 result = add.delay(4, 4)
-print(dir(result))
-print("state")
+#print(dir(result))
+#print("state", result.ready())
+
+def on_raw_message(body):
+    print(body)
+    
+print(result.get(on_message=on_raw_message, propagate=False))
+
+
 #print(result.ready())
 
 # for r in result.collect():
--- tasks.py
+++ tasks.py
@@ -3,16 +3,38 @@
 from time import sleep
 from celery import Celery
 from celery.utils.log import get_task_logger
+import subprocess
 
 logger = get_task_logger(__name__)
 
 app = Celery('tasks', backend='rpc://', result_persistent = True
 ,broker='amqp://guest:guest@localhost:5672')
 
-@app.task
-def add(x, y):
+
+app.conf.update(    
+    result_serializer='json'  
+)
+
+print(dir(app))
+
+print('logger dr')
+print(dir(logger.log))
+
+
+@app.task(ignore_result=False, bind=True)
+def add(self, x, y):
+
+    # redirect_stdouts_to_logger
+
     logger.info("Adding %s + %s" % (x, y))
     sleep(4)
     print("executing stuff")
+
+    a = subprocess.check_output(["ls", "-l"])
+    self.update_state(state="PROGRESS", meta=a.decode("utf-8"))
+
+    print(a)
+    
+
     return x + y
 

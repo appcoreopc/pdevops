afeb542dedf0351e72d86dee78093ffbb5a0a21d
jeremy woo <kepungmoin@hotmail.com>
2019-10-07 19:00:22 +1300

    add

--- exec.py
+++ exec.py
@@ -1,3 +1,10 @@
 from tasks import add
 
-add.delay(4, 4)
+result = add.delay(4, 4)
+print(dir(result))
+print("state")
+#print(result.ready())
+
+# for r in result.collect():
+#     print("state")
+#     print(r.state)
--- tasks.py
+++ tasks.py
@@ -1,12 +1,17 @@
 # docker run -d -p 5672:5672 rabbitmq
+
 from time import sleep
 from celery import Celery
+from celery.utils.log import get_task_logger
 
-app = Celery('tasks', broker='amqp://guest:guest@localhost:5672')
+logger = get_task_logger(__name__)
 
+app = Celery('tasks', backend='rpc://', result_persistent = True
+,broker='amqp://guest:guest@localhost:5672')
 
 @app.task
 def add(x, y):
+    logger.info("Adding %s + %s" % (x, y))
     sleep(4)
     print("executing stuff")
     return x + y

4d9ca782ff06a2b90b076cd2b1d93d4f59d4deae
jeremy woo <kepungmoin@hotmail.com>
2019-10-11 17:24:19 +1300

    addd app constant

--- BuildProcessor.py
+++ BuildProcessor.py
@@ -1,11 +1,16 @@
 from messageQueue.RabbitMqComponent import RabbitMqComponent
 from messageQueue.QueueManager import QueueManager
+from AppConstants import BUILDREQUESTQUEUE
 
 # sudo docker run -p 5672:5672 -p 15672:15672 -d --hostname my-rabbit --name some-rabbit rabbitmq:3-management
 
 ## Setup 
 
-queueTransport = RabbitMqComponent("localhost", "hello")
+class Test:
+    def __init__(self):
+        print("init")
+
+queueTransport = RabbitMqComponent("localhost", BUILDREQUESTQUEUE)
 
 queueManager = QueueManager(queueTransport)
 queueManager.publish("testing testing")

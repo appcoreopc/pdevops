35fc6300c00345379cb27e256aa4969c631cbb48
jeremy woo <kepungmoin@hotmail.com>
2019-10-13 16:38:20 +1300

    fix qeue routing issues.

--- messageQueue/RabbitMqComponent.py
+++ messageQueue/RabbitMqComponent.py
@@ -24,10 +24,12 @@ class RabbitMqComponent(QueueComponent):
         self.channel.basic_publish(exchange=self.queueType.targetName, routing_key='', body=message)
 
     def read(self):
-        print('setting readvalue')    
+        print('setting read queue for fan out, ', self.queueType.targetName)    
         result = self.channel.queue_declare(queue='', exclusive=True)
         queue_name = result.method.queue
+        print("randon queue name", queue_name)
         self.channel.queue_bind(exchange=self.queueType.targetName, queue=queue_name)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=self.receiveMessageHandler, auto_ack=True)
         self.channel.start_consuming()
         
     def _createQueue(self, targetqueue : str):

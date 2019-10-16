5f3d64ad4cd77ddb5face11a59db3bef9d45f62e
jeremy woo <kepungmoin@hotmail.com>
2019-10-16 18:26:22 +1300

    added post statements

--- RestService/BuildRequestService.py
+++ RestService/BuildRequestService.py
@@ -1,3 +1,5 @@
+import json
+import falcon
 
 class BuildRequestService:
 
@@ -11,4 +13,28 @@ class BuildRequestService:
             'author': 'Grace Hopper'
         }
         resp.media = quote
+
+    def on_post(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'POST'
+        }
+        resp.media = quote
+
+    def on_put(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'PUT'
+        }
+        resp.media = quote
+
+    
 
